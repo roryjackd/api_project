db172667581354ecd0b4c2f611166a40f503a551
roryjackd <roryjackd@yahoo.com>
2017-05-09 22:34:19 -0600

    second

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get('/results', function (req, res) {
+  request("http://www.omdbapi.com/?s=iowa", function(error, response, body){
+      if(!error && response.statusCode == 200){
+          var data = JSON.parse(body)
+          res.render("results", {data: data});
+      }
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
